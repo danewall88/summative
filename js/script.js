cd4985e417becdf1b236b600958df8a8155b84cb
danewall88 <danebrendynwall@gmail.com>
2019-04-10 11:37:55 +1200

    added a javsscript file, worked on the home page and set everything up

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var key = '9uzrs7VpfguqgaX5evogUGwxsjAQeOTa';	
+	var behanceUser = 'TAK-Design'; 				
+
+	var urlProjects = 'https://api.behance.net/v2/users/' + behanceUser + '/projects?client_id=' + key;
