d31041104832100f7e13030b2f721524088daa41
danewall88 <danebrendynwall@gmail.com>
2019-04-11 11:53:35 +1200

    added some javascript

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@ var key = '9uzrs7VpfguqgaX5evogUGwxsjAQeOTa';
 	var behanceUser = 'TAK-Design'; 				
 
 	var urlProjects = 'https://api.behance.net/v2/users/' + behanceUser + '/projects?client_id=' + key;
+
+	var designersList = {[
+		{
+		name: 'one',
+		img: 'img1.png',
+		quote: 'insert quote one'
+		}
+		{
+		name: 'two',
+		img:'img2.png',
+		quote: 'insert quote two'
+		}
+		{
+		name: 'three',
+		img: 'img3.png',
+		quote: 'insert quote three'
+		}
+		{
+		name: 'four',
+		img: 'img4.png',
+		quote: 'insert quote four'
+		}
+		]}
